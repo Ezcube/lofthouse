edcd7d0a3e07f1a9229b25a862d0e63d1990a5dc
Ezcube <32035240+Ezcube@users.noreply.github.com>
2020-07-19 21:46:05 +0300

    Update Hero.jsx

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -26,17 +26,17 @@ const Header = () => {
       <Container>
         <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={500} distance="30px">
           <h1 className="hero-title">
-            {title || 'Hi, my name is'}{' '}
-            <span className="text-color-main">{name || 'Your Name'}</span>
+            {title || 'Привет, меня зовут'}{' '}
+            <span className="text-color-main">{name || 'Артур'}</span>
             <br />
-            {subtitle || "I'm the Unknown Developer."}
+            {subtitle || "Компания Loft House не сдает мне дом с 2018 года."}
           </h1>
         </Fade>
         <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={1000} distance="30px">
           <p className="hero-cta">
             <span className="cta-btn cta-btn--hero">
               <Link to="about" smooth duration={1000}>
-                {cta || 'Know more'}
+                {cta || 'Подробнее'}
               </Link>
             </span>
           </p>
